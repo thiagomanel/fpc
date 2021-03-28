b14595cbdaecd7e2a2135644e8de554f7d544496
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2021-03-28 15:09:43 -0300

    Use RW mutex instead of channel

--- LittleBookOfSemaphores/chapter3/mutex/go/main.go
+++ LittleBookOfSemaphores/chapter3/mutex/go/main.go
@@ -7,31 +7,30 @@ import (
 )
 
 func main() {
-	sem := make(chan bool, 1)
-	sem <- true
+	mutex := &sync.RWMutex{}
 	end := &sync.WaitGroup{}
 	end.Add(2)
 	count := int32(0)
-	go threadA(&count, sem, end)
-	go threadB(&count, sem, end)
+	go threadA(&count, mutex, end)
+	go threadB(&count, mutex, end)
 	end.Wait()
 	fmt.Printf("Count: %d\n", atomic.LoadInt32(&count))
 }
 
-func threadA(count *int32, sem chan bool, end *sync.WaitGroup) {
+func threadA(count *int32, mutex *sync.RWMutex, end *sync.WaitGroup) {
 	for i := 0; i < 1000000; i++ {
-		<- sem
+		mutex.Lock()
 		atomic.AddInt32(count, 1)
-		sem <- true
+		mutex.Unlock()
 	}
 	end.Done()
 }
 
-func threadB(count *int32, sem chan bool, end *sync.WaitGroup) {
+func threadB(count *int32, mutex *sync.RWMutex, end *sync.WaitGroup) {
 	for i := 0; i < 1000000; i++ {
-		<- sem
+		mutex.Lock()
 		atomic.AddInt32(count, 1)
-		sem <- true
+		mutex.Unlock()
 	}
 	end.Done()
 }
