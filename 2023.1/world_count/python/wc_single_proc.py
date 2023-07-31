5994ca54a3c5ea299aafe0b0a5b2add321fce2db
JUAN SILVA <54045471+dderking@users.noreply.github.com>
2023-07-31 10:05:45 -0300

    removing blank lines

--- 2023.1/world_count/python/wc_single_proc.py
+++ 2023.1/world_count/python/wc_single_proc.py
@@ -14,22 +14,18 @@ def wc_file(filename):
 
 def wc_dir(dir_path):
 	count = 0
-
 	for filename in os.listdir(dir_path):
 		filepath = os.path.join(dir_path, filename)
 		if os.path.isfile(filepath):
 			count += wc_file(filepath)
-
 	return count
 
 def main():
 	if len(sys.argv) != 2:
 		print("Usage: python ", sys.argv[0], "root_directory_path")
 		return
-
 	root_path = os.path.abspath(sys.argv[1])
 	count = wc_dir(root_path)
-
 	print(count)
 
 if __name__ == "__main__":
