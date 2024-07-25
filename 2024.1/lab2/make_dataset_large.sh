e2b8680f9af4b07d43b92339bb06c796fef1b030
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-07-24 21:26:13 -0300

    add make_dataset_large.sh

--- 2024.1/lab2/make_dataset_large.sh
+++ 2024.1/lab2/make_dataset_large.sh
@@ -8,6 +8,6 @@ mkdir -p dataset
 for i in `seq 1 $nfiles`
 do
 	# Generate a random size between 50k and 72k
-    file_size=$RANDOM
+    file_size=$((1000000 + RANDOM % 22001))
 	head -c $file_size < /dev/urandom > dataset/file.$i
 done
