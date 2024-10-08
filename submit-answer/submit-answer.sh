7c0502e051caabdf5992a0deaec818944ada1bed
giovannifs <giovanni@lsd.ufcg.edu.br>
2024-10-08 11:31:18 -0300

    Changing script to allow specifyng the type og the test that is being submitted.

--- submit-answer/submit-answer.sh
+++ submit-answer/submit-answer.sh
@@ -3,7 +3,8 @@
 SERVER_ADRESS=150.165.85.31
 SERVER_USER="aluno"
 
-FILE_PATH=$1
+TYPE=$1
+FILE_PATH=$2
 
 # checking if the file exists
 if [ ! -f $FILE_PATH ]; then
@@ -11,7 +12,28 @@ if [ ! -f $FILE_PATH ]; then
    exit 1
 fi
 
-scp $FILE_PATH $SERVER_USER@$SERVER_ADRESS:/home/$SERVER_USER/answers/prova2/
+if [ $TYPE = "prova1" ]
+then
+        echo "Repondo prova 1"
+	scp $FILE_PATH $SERVER_USER@$SERVER_ADRESS:/home/$SERVER_USER/answers/reposicao/prova1/
+
+else 
+	if [ $TYPE = "prova2" ]
+	then
+        	echo "Repondo prova2"
+		 scp $FILE_PATH $SERVER_USER@$SERVER_ADRESS:/home/$SERVER_USER/answers/reposicao/prova2/
+	else 
+		if [ $TYPE = "prova3" ]
+		then 
+			echo "Repondo prova3"
+			 scp $FILE_PATH $SERVER_USER@$SERVER_ADRESS:/home/$SERVER_USER/answers/reposicao/prova3/
+		else
+			echo "ERROR: Invalid test option!"
+		fi
+	fi
+fi
+
+#scp $FILE_PATH $SERVER_USER@$SERVER_ADRESS:/home/$SERVERi_USER/answers/prova2/
  	
 EXIT_CODE=$?
 if [ $EXIT_CODE -eq 0 ];
